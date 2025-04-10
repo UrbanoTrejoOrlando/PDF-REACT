09aca5e71200b9ed8790368ccae16aadecaafac8
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-04-10 08:25:43 -0600

    Base de datos

---
+++ API-Productos/db.js
@@ -0,0 +1,78 @@
+module.exports.data = [
+    {
+        name:"sabritas",
+        price:"20",
+        category:"comida",
+    },
+    {
+        name:"cheetos",
+        price:"18",
+        category:"comida",
+    },
+    {
+        name:"doritos",
+        price:"15",
+        category:"comida",
+    },
+    {
+        name:"jamon",
+        price:"25",
+        category:"comida",
+    },
+    {
+        name:"galletas",
+        price:"25",
+        category:"comida",
+    },
+    {
+        name:"playera",
+        price:"85",
+        category:"Ropa hombre",
+    },
+    {
+        name:"calcetines",
+        price:"50",
+        category:"Ropa hombre",
+    },
+    {
+        name:"sudaderas",
+        price:"125",
+        category:"Ropa hombre",
+    },
+    {
+        name:"pantalon",
+        price:"220",
+        category:"Ropa hombre",
+    },
+    {
+        name:"playera roja",
+        price:"25",
+        category:"Ropa hombre",
+    },
+    {
+        name:"suavitel",
+        price:"50",
+        category:"Limpieza",
+    },
+    {
+        name:"fabuloso",
+        price:"40",
+        category:"Limpieza",
+    },
+    {
+        name:"cloro",
+        price:"30",
+        category:"Limpieza",
+    },
+    {
+        name:"pino",
+        price:"55",
+        category:"Limpieza",
+    },
+    {
+        name:"Jabon",
+        price:"60",
+        category:"Limpieza",
+    },
+    
+]
